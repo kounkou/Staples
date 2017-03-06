ae90de04d7ff1cf6963633818c413cc7439cf09d
godbod <gkounkou@gmail.com>
2017-03-06 23:19:16 +0100

    Modify webserver and add a data file

--- scripts/webserver.sh
+++ scripts/webserver.sh
@@ -1,6 +1,48 @@
 #!/bin/bash
+#---------------------------------
+#
+# FILE : webserver.sh  
+#
+# DESCRITION :
+# webserver script is a server side application
+# The server application will send the 
+# Json data to the client application
+#
+#---------------------------------
 
-while true
-do 
-  echo -e "HTTP/1.1 200 OK\n\n {"example":"value"}" | nc -l -p 1500 -q 1
-done
+USAGE="Usage : command"
+DATA_PATH="/tmp/"
+
+# generate_data function
+generate_data()
+{
+   echo 'Enter : function generate_data()'
+   if [ ! -f ${DATA_PATH}/data ]
+   then
+      cp -rf data /tmp/
+   fi
+   echo 'Exit  : function generate_data()'
+}
+
+# launch_server function
+launch_server()
+{
+   echo 'Enter : function launch_server()'
+   while true
+   do 
+      information=`cat ${DATA_PATH}/data`
+      echo -e "HTTP/1.1 200 OK\n\n ${information}" | nc -l -p 1500 -q 1
+   done
+   echo 'Exit  : function launch_server()'
+}
+
+if [ $# != 0 ]
+then
+   echo $USAGE
+   exit 1
+fi
+
+#---------------------------------
+generate_data
+launch_server
+
