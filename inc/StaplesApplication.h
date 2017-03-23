74ae900b1d4d54063421489fab44d28aa2c0d292
Jacques <gkounkou@gmail.com>
2017-03-23 15:38:14 +0100

    Updating the view element
    Updating the view element for the StaplesApplication class

--- inc/StaplesApplication.h
+++ inc/StaplesApplication.h
@@ -21,6 +21,7 @@
 #include <QtQuick/QQuickWindow>
 #include <QtQml/QQmlContext>
 #include <QtQml/QQmlComponent>
+#include <QQuickView>
 
 #include "StaplesModel.h"
 
@@ -48,6 +49,7 @@ private:
    QQmlContext*           _childContext;
    QQmlComponent*         _component;
    StaplesModel*          _model;
+   QQuickView*            _view;
 };
 
 #endif // end of _STAPLES_APPLICATION_H_
