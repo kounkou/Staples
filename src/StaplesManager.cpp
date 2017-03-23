44d03941c17a52a40ef6280325841490eca447fd
godbod <gkounkou@gmail.com>
2017-03-23 23:37:14 +0100

    Staples application is now served with list of Staples from a data base, and a basic Rasbpberry pi server

--- src/StaplesManager.cpp
+++ src/StaplesManager.cpp
@@ -2,6 +2,8 @@
 #include "StaplesManager.h"
 #include "StaplesModel.h"
 
+#include <sstream>
+
 StaplesManager::StaplesManager()
 {
 }
@@ -31,22 +33,22 @@ int StaplesManager::retrieveStaples(const QJsonDocument& doc)
     {
         qDebug() << "status is ok";
 
-        // adding fake elements
-        _model->addStaple(20170318, "Leerdammer",  6.52);
-        _model->addStaple(20170318, "Ducros",      3.04);
-        _model->addStaple(20170318, "Evian",       1.85);
-        _model->addStaple(20170318, "Lactel",      2.82);
-        _model->addStaple(20170318, "Daddie",      2.04);
-        _model->addStaple(20170318, "some elt 1",  6.52);
-        _model->addStaple(20170318, "some elt 2",  3.04);
-        _model->addStaple(20170318, "some elt 3",  1.85);
-        _model->addStaple(20170318, "some elt 4",  2.82);
-        _model->addStaple(20170318, "some elt 5",  2.04);
-        _model->addStaple(20170318, "some elt 6",  6.52);
-        _model->addStaple(20170318, "some elt 7",  3.04);
-        _model->addStaple(20170318, "some elt 8",  1.85);
-        _model->addStaple(20170318, "some elt 9",  2.82);
-        _model->addStaple(20170318, "some elt A",  2.04);
+        QVariantMap staplesMap = jsonObj.toVariantMap();
+        QVariantMap dataMap    = staplesMap["data"].toMap();
+        QVariantMap staples    = dataMap["staples"].toMap();
+
+        /*
+         * This loop will iterate through the elements of the data
+         * file, and staples inside the listview
+         */
+        for (int ii = 1; ii < staples.size(); ++ii)
+        {
+            QString ss = QString("s%1").arg(ii);
+
+            _model->addStaple((staples[ss].toMap())["exp"].toInt(),
+                              (staples[ss].toMap())["name"].toString().toStdString(),
+                              (staples[ss].toMap())["price"].toFloat());
+        }
 
         status = 0;
     }
